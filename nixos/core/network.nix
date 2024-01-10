cbf674170721320df17b3645e4ab7f2f58e0bfb6
Chris Zervakis <xrist.zerv@protonmail.com>
2024-01-11 00:19:21 +0200

    network: TCP/IP stack optimizations

--- nixos/core/network.nix
+++ nixos/core/network.nix
@@ -35,4 +35,21 @@
   };
 
   systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
+
+  ## TCP/IP stack optimizations
+  boot = {
+    kernel.sysctl = {
+      # TCP Fast Open is a TCP extension that reduces network latency by packing
+      # data in the sender’s initial TCP SYN. Use it for both incoming and
+      # outgoing connections
+      "net.ipv4.tcp_fastopen" = 3;
+
+      # Use the BBR congestion control algorithm which can help achieve
+      # higher bandwidths and lower latencies
+      # https://github.com/google/bbr/
+      "net.ipv4.tcp_congestion_control" = "bbr";
+      "net.core.default_qdisc" = "cake";
+    };
+    kernelModules = ["tcp_bbr"];
+  };
 }
