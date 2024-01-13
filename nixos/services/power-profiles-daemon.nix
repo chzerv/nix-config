b5149cedb57bf6bc5543bd1db706b40f44d6db33
Chris Zervakis <xrist.zerv@protonmail.com>
2024-01-13 23:10:36 +0200

    fix: power-profiles-daemon udev rules
    TIL that udev cannot access systemPackages and thus, we need to refer
    to the script directly, which is also why `writeShellScript` is used
    instead of `writeShellScriptBin`

--- nixos/services/power-profiles-daemon.nix
+++ nixos/services/power-profiles-daemon.nix
@@ -6,13 +6,11 @@
 }: let
   opts = config.local.sys;
 
-  enable-power-saving = pkgs.writeShellScriptBin "enable-power-saving" ''
+  enable-power-saving = pkgs.writeShellScript "enable-power-saving" ''
     if [[ $1 == true ]]; then
-      /usr/bin/powerprofilesctl set power-saver &&
-        echo 0 >/sys/devices/system/cpu/cpufreq/boost
+      ${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver
     elif [[ $1 == false ]]; then
-      /usr/bin/powerprofilesctl set balanced &&
-        echo 1 >/sys/devices/system/cpu/cpufreq/boost
+      ${pkgs.power-profiles-daemon}/bin/powerprofilesctl set balanced
     else
       echo "Unknown argument" >&2
     fi
@@ -22,11 +20,9 @@ in {
   config = lib.mkIf opts.services.ppd {
     services.power-profiles-daemon.enable = true;
 
-    environment.systemPackages = [enable-power-saving];
-
     services.udev.extraRules = ''
-      SUBSYSTEM=="power_supply", ATTR{online}=="0", RUN+="enable-power-saving true"
-      SUBSYSTEM=="power_supply", ATTR{online}=="1", RUN+="enable-power-saving false"
+      SUBSYSTEM=="power_supply", ATTR{online}=="0", RUN+="${enable-power-saving} true"
+      SUBSYSTEM=="power_supply", ATTR{online}=="1", RUN+="${enable-power-saving} false"
     '';
   };
 }
