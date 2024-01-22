629b47fe21a207c7cc7b85e45dcbf43985117cf1
Chris Zervakis <xrist.zerv@protonmail.com>
2024-01-22 14:02:08 +0200

    feat(nvim, alacritty): Add transparency

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -102,6 +102,15 @@ return {
         name = "catppuccin",
         priority = 1000,
         config = function()
+            require("catppuccin").setup({
+                transparent_background = true,
+                dim_inactive = {
+                    enabled = true,
+                    shade = "dark",
+                    percentage = 0.15, -- percentage of the shade to apply to the inactive window
+                },
+            })
+
             vim.cmd.colorscheme("catppuccin-mocha")
         end,
     },
