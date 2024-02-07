cf476a2912027bb974f1d203bc100d46742d7af7
Chris Zervakis <xrist.zerv@protonmail.com>
2024-02-07 15:47:31 +0200

    refactor(sops-nix): Use mkDefault for age.keyFile & look for SSH keys
    When deploying to a new host, having `sops-nix` automatically create
    AGE keys from SSH keys instead of specifying an AGE key can be very
    handy

--- nixos/core/sops.nix
+++ nixos/core/sops.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   username,
   hostname,
   ...
@@ -6,6 +7,10 @@
   sops = {
     defaultSopsFile = ../../hosts/${hostname}/secrets.yaml;
     defaultSopsFormat = "yaml";
-    age.keyFile = "/home/${username}/.config/sops/age/keys.txt";
+    # Allow overriding `age.keyFile`
+    # This is especially useful when we are deploying to a new machine and we want to use
+    # it's SSH keys as AGE keys
+    age.keyFile = lib.mkDefault "/home/${username}/.config/sops/age/keys.txt";
+    age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
   };
 }
