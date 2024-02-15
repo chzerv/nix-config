799422017cd6c1acd23727e8957be42443ff3277
Chris Zervakis <xrist.zerv@protonmail.com>
2024-02-15 19:46:32 +0200

    fix(nvim): Markdown header folding

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -1,8 +1,19 @@
 vim.opt_local.spell = true
 vim.opt_local.spelllang = "en_us"
-vim.opt_local.foldmethod = "manual"
+vim.opt_local.foldlevel = 1
 vim.opt_local.conceallevel = 2
 vim.opt_local.formatoptions:append("t")
+
+vim.g.vim_markdown_folding_disabled = false
+vim.g.vim_markdown_folding_style_pythonic = 1
+vim.g.vim_markdown_conceal = false
+vim.g.vim_markdown_conceal_code_blocks = false
+vim.g.vim_markdown_frontmatter = true
+vim.g.vim_markdown_toml_frontmatter = false
+vim.g.vim_markdown_json_frontmatter = false
+vim.g.vim_markdown_no_extensions_in_markdown = false
+vim.g.vim_markdown_follow_anchor = true
+vim.g.vim_markdown_new_list_item_indent = 2
 
 -- https://github.com/kylechui/nvim-surround/discussions/53
 local ok, surround = pcall(require, "nvim-surround")
@@ -43,13 +54,3 @@ if ok then
         },
     })
 end
-
-vim.g.vim_markdown_folding_disabled = true
-vim.g.vim_markdown_conceal = true
-vim.g.vim_markdown_conceal_code_blocks = false
-vim.g.vim_markdown_frontmatter = true
-vim.g.vim_markdown_toml_frontmatter = false
-vim.g.vim_markdown_json_frontmatter = false
-vim.g.vim_markdown_no_extensions_in_markdown = false
-vim.g.vim_markdown_follow_anchor = true
-vim.g.vim_markdown_new_list_item_indent = 2
