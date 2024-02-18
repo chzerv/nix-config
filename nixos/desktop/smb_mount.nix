8fc552a1442d50edf45133a105ca2c54c22ea9b1
Chris Zervakis <xrist.zerv@protonmail.com>
2024-02-18 11:37:40 +0200

    fix: Add the 'nofail' mount option to the SMB mount

--- nixos/desktop/smb_mount.nix
+++ nixos/desktop/smb_mount.nix
@@ -33,7 +33,7 @@ in {
       device = "//truenas/data";
       fsType = "cifs";
       options = let
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,nofail";
       in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
     };
   };
