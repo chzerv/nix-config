7775557bec6e215c8ae5772a7796db955a092e14
Chris Zervakis <xrist.zerv@protonmail.com>
2024-02-23 16:33:53 +0200

    feat(nvim): Markdown callout nippet and folding

--- config/nvim/lua/plugins/snippets/markdown.lua
+++ config/nvim/lua/plugins/snippets/markdown.lua
@@ -22,4 +22,21 @@ ls.add_snippets("markdown", {
             { i(1), i(2), i(3), i(4) }
         )
     ),
+    s(
+        "callout",
+        fmt(
+            [[
+    > [!{}] {}
+    > {}
+
+    {}
+    ]],
+            {
+                c(1, { t("Note"), t("Tip"), t("Info"), t("Warning"), t("Danger"), t("Example") }),
+                i(2),
+                i(3),
+                i(4),
+            }
+        )
+    ),
 })
