5223ca5522b756a3f1402f36a028fd7e720c4cda
Chris Zervakis <xrist.zerv@protonmail.com>
2024-02-27 12:03:20 +0200

    fix: Make flatpaks able to access system fonts via bindfs

--- nixos/desktop/flatpak.nix
+++ nixos/desktop/flatpak.nix
@@ -15,5 +15,31 @@ in {
         hash = "sha256-M3HdJQ5h2eFjNjAHP+/aFTzUQm9y9K+gwzc64uj+oDo=";
       };
     };
+
+    # Fix for Flatpak apps not being able to access system fonts
+    # https://nixos.wiki/wiki/Fonts
+    system.fsPackages = [pkgs.bindfs];
+    fileSystems = let
+      mkRoSymBind = path: {
+        device = path;
+        fsType = "fuse.bindfs";
+        options = ["ro" "resolve-symlinks" "x-gvfs-hide"];
+      };
+
+      aggregatedIcons = pkgs.buildEnv {
+        name = "system-icons";
+        paths = with pkgs; [gnome.gnome-themes-extra];
+        pathsToLink = ["/share/icons"];
+      };
+
+      aggregatedFonts = pkgs.buildEnv {
+        name = "system-fonts";
+        paths = config.fonts.packages;
+        pathsToLink = ["/share/fonts"];
+      };
+    in {
+      "/usr/share/icons" = mkRoSymBind "${aggregatedIcons}/share/icons";
+      "/usr/local/share/fonts" = mkRoSymBind "${aggregatedFonts}/share/fonts";
+    };
   };
 }
