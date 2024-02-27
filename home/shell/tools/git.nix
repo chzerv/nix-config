60e481a8904cb1c20cd57f16d521fd82a69876dd
Chris Zervakis <xrist.zerv@protonmail.com>
2024-02-27 19:39:22 +0200

    refactor(git): Merge git and lazygit modules

--- home/shell/tools/git.nix
+++ home/shell/tools/git.nix
@@ -1,47 +1,63 @@
 {
-  programs.git = {
-    enable = true;
-    delta.enable = true;
-    userName = "Chris Zervakis";
-    userEmail = "xrist.zerv@protonmail.com";
-    aliases = {
-      st = "status";
-      co = "checkout";
-      cm = "commit --verbose";
-      ca = "commit --amend";
-      can = "commit --amend --no-edit";
-      graph = "log --oneline --graph --all --decorate";
-      lg = "log --oneline";
-      find = "!f() { cd `git rev-parse --show-toplevel` && git ls-files | rg \"$@\"; }; f";
-      # Show the most recent commit matching a query
-      query = "!f() { git show :/\"$@\"; }; f";
+  programs = {
+    git = {
+      enable = true;
+      delta.enable = true;
+      userName = "Chris Zervakis";
+      userEmail = "xrist.zerv@protonmail.com";
+      aliases = {
+        st = "status";
+        co = "checkout";
+        cm = "commit --verbose";
+        ca = "commit --amend";
+        can = "commit --amend --no-edit";
+        graph = "log --oneline --graph --all --decorate";
+        lg = "log --oneline";
+        find = "!f() { cd `git rev-parse --show-toplevel` && git ls-files | rg \"$@\"; }; f";
+        # Show the most recent commit matching a query
+        query = "!f() { git show :/\"$@\"; }; f";
+        unstage = "reset HEAD --"; # git unstage path/to/file
+      };
+      extraConfig = {
+        commit.verbose = true;
+        init.defaultBranch = "main";
+        rerere.enabled = true;
+        core.editor = "nvim";
+        delta = {
+          features = "line-numbers decorations";
+          whitespace-error-style = "22 reverse";
+          syntax-theme = "TwoDark";
+          side-by-side = "false";
+        };
+      };
+      ignores = [
+        ".DS_Store"
+        ".svn"
+        "*~"
+        "*.swp"
+        "*.orig"
+        "*.rbc"
+        "*.iml"
+        ".project"
+        ".settings"
+        ".vscode/"
+        "*.auto.tfvars"
+        "*.log"
+        ".env"
+        ".direnv"
+        ""
+      ];
     };
-    extraConfig = {
-      commit.verbose = true;
-      init.defaultBranch = "main";
-      rerere.enabled = true;
-      core.editor = "nvim";
-      delta = {
-        features = "line-numbers decorations";
-        whitespace-error-style = "22 reverse";
-        syntax-theme = "TwoDark";
-        side-by-side = "false";
+    lazygit = {
+      enable = true;
+      settings = {
+        git = {
+          paging = {
+            colorArg = "always";
+            pager = "delta --dark --paging=never";
+          };
+        };
       };
     };
-    ignores = [
-      ".DS_Store"
-      ".svn"
-      "*~"
-      "*.swp"
-      "*.orig"
-      "*.rbc"
-      "*.iml"
-      ".project"
-      ".settings"
-      ".vscode/"
-      "*.auto.tfvars"
-      "*.log"
-      ".env"
-    ];
   };
 }
