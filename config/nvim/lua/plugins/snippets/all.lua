b43a743e22ef4d324d67c478d9d08811ba2bb49b
Chris Zervakis <xrist.zerv@protonmail.com>
2024-03-10 20:43:38 +0200

    feat(nvim): Snippet tweaks

--- config/nvim/lua/plugins/snippets/all.lua
+++ config/nvim/lua/plugins/snippets/all.lua
@@ -1,25 +1,12 @@
 local ls = require("luasnip")
 local s = ls.snippet
-local sn = ls.snippet_node
 local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
 local c = ls.choice_node
-local d = ls.dynamic_node
-local r = ls.restore_node
-local l = require("luasnip.extras").lambda
-local rep = require("luasnip.extras").rep
-local p = require("luasnip.extras").partial
-local m = require("luasnip.extras").match
-local n = require("luasnip.extras").nonempty
-local dl = require("luasnip.extras").dynamic_lambda
-local fmt = require("luasnip.extras.fmt").fmt
-local fmta = require("luasnip.extras.fmt").fmta
-local types = require("luasnip.util.types")
-local conds = require("luasnip.extras.expand_conditions")
 
 local function fd_cmt_string(cmt)
-    return string.format(vim.bo.commentstring, " " .. cmt)
+    return string.format(vim.bo.commentstring, "" .. cmt)
 end
 
 -- Simple placeholders
@@ -31,13 +18,14 @@ ls.add_snippets("all", {
         end)
     ),
     s(
-        "td",
+        "todo",
         c(1, {
             t(fd_cmt_string("TODO: ")),
             t(fd_cmt_string("FIXME: ")),
-            t(fd_cmt_string("BUG: ")),
         }),
         i(0)
     ),
-    s("ft", t("vi: set ft=yaml :")),
+    s("ft", t(fd_cmt_string("vi: set ft=yaml :"))),
+
+    s("shebang", c(1, { t("#!/usr/bin/env bash"), t("#!/usr/bin/env fish") })),
 })
--- config/nvim/lua/plugins/snippets/sh.lua
+++ config/nvim/lua/plugins/snippets/sh.lua
@@ -7,16 +7,17 @@ local fmt = require("luasnip.extras.fmt").fmt
 
 -- Simple placeholders
 ls.add_snippets("sh", {
-    s("#!", t("#!/usr/bin/env bash")),
-    s("shebang", t("#!/usr/bin/env bash")),
+
     ls.parser.parse_snippet("if", "if [[ ${1:cond} ]]; then\n\t${0:body}\nfi"),
+
     ls.parser.parse_snippet("elif", "elif [[ ${1:cond} ]]; then\n\t${0:body}"),
+
     s(
         "command_exists",
         fmt(
             [[
-        if ! command -v {} &>/dev/null; then 
-            echo "{} not installed." 
+        if ! command -v {} &>/dev/null; then
+            echo "{} not installed."
             exit 1
         fi
         ]],
