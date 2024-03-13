2a8a81d51d0d86f5d6e96ec4d4e548d0fa42c2e6
Chris Zervakis <xrist.zerv@protonmail.com>
2024-03-13 21:15:44 +0200

    feat(nvim); ansiblells configuration tweaks

--- config/nvim/lua/plugins/lsp/servers/ansiblels.lua
+++ config/nvim/lua/plugins/lsp/servers/ansiblels.lua
@@ -1,8 +1,26 @@
 return {
+    filetypes = {
+        "yaml.ansible",
+    },
     settings = {
         ansible = {
+            ansible = {
+                path = "ansible",
+                useFullyQualifiedCollectionNames = true,
+            },
+            ansibleLint = {
+                enabled = true,
+                path = "ansible-lint",
+            },
             executionEnvironment = {
                 enabled = false,
+            },
+            python = {
+                interpreterPath = "python",
+            },
+            completion = {
+                provideRedirectModules = true,
+                provideModuleOptionAliases = true,
             },
             validation = {
                 enabled = true,
