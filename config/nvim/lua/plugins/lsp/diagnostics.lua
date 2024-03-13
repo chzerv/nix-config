656ebec7475dfacdbf44853a0df8ea2d915bb551
Chris Zervakis <xrist.zerv@protonmail.com>
2024-03-13 21:13:34 +0200

    refactor(nvim): Remove unneeded diagnostic keymaps

--- config/nvim/lua/plugins/lsp/diagnostics.lua
+++ config/nvim/lua/plugins/lsp/diagnostics.lua
@@ -1,18 +1,5 @@
 local M = {}
 
--- Credits to LazyVim:
--- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
-local function goto_diagnostic(next, severity)
-    -- Choose which diagnostic to jump to - the next or previous one
-    local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
-
-    severity = severity and vim.diagnostic.severity[severity]
-
-    return function()
-        go({ severity = severity })
-    end
-end
-
 M.setup = function()
     vim.diagnostic.config({
         -- Don't show diagnostics at the end of the line
@@ -22,7 +9,6 @@ M.setup = function()
             format = nil,
             prefix = "",
         },
-        signs = true,
         underline = true,
         update_in_insert = false,
         severity_sort = true,
@@ -62,12 +48,8 @@ M.setup = function()
         { desc = "Open diagnostics in Telescope" }
     )
 
-    vim.keymap.set("n", "]d", goto_diagnostic(true), { desc = "Next diagnostic" })
-    vim.keymap.set("n", "[d", goto_diagnostic(false), { desc = "Previous diagnostic" })
-    vim.keymap.set("n", "]e", goto_diagnostic(true, "ERROR"), { desc = "Next error" })
-    vim.keymap.set("n", "[e", goto_diagnostic(false, "ERROR"), { desc = "Previous error" })
-    vim.keymap.set("n", "]w", goto_diagnostic(true, "WARN"), { desc = "Next warning" })
-    vim.keymap.set("n", "[w", goto_diagnostic(false, "WARN"), { desc = "Previous warning" })
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
 end
 
 return M
