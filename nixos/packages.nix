56f232032ef1e1221b6816df7d0052ef4e9b7d30
Chris Zervakis <xrist.zerv@protonmail.com>
2024-03-25 10:25:02 +0200

    feat(packages): Install server-specific packages

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -5,25 +5,36 @@
   type,
   ...
 }: {
-  environment.systemPackages = with pkgs; [
-    vim
-    rsync
-    git
-    libnotify
-    ntfs3g
-    cifs-utils
-    file
-    killall
-    usbutils
-    lm_sensors
-    pciutils
-    man-pages
-    iperf3
-    lsof
-    wireguard-tools
-    pv
-    cachix
-  ];
+  environment.systemPackages = with pkgs;
+    [
+      vim
+      rsync
+      git
+      libnotify
+      ntfs3g
+      cifs-utils
+      file
+      killall
+      usbutils
+      lm_sensors
+      pciutils
+      man-pages
+      iperf3
+      lsof
+      wireguard-tools
+      pv
+      cachix
+    ]
+    ++ lib.optionals (type == "server") [
+      util-linux # device info: lscpu, lsblk and more
+      sysstat # iostat, sar and more
+      tcpdump # network sniffer
+      bcc # bcc eBPF tools for performance analysis, tracing and more
+
+      # required for using DNS options in `tcpconnect`
+      python312Packages.dnslib
+      python312Packages.cachetools
+    ];
 
   programs.wireshark = {
     enable = type != "server";
