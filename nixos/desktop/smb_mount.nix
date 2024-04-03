c5475d70ac0bb21f582620d77f7ff4e797aafd22
Chris Zervakis <xrist.zerv@protonmail.com>
2024-04-03 17:58:46 +0300

    fix(smb_mount): SMB share not getting auto mounted
    Hopefully, using an IP address instead of hostname for the server
    and the `_netdev` mount option (tells the share to mount after
    there is a network connection) fixes the issue

--- nixos/desktop/smb_mount.nix
+++ nixos/desktop/smb_mount.nix
@@ -30,10 +30,10 @@ in {
     ];
 
     fileSystems."/media/smb" = {
-      device = "//truenas/data";
+      device = "//192.168.1.11/data";
       fsType = "cifs";
       options = let
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,nofail";
+        automount_opts = "_netdev,x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,nofail";
       in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
     };
   };
