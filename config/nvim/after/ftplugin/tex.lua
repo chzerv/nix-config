ddd0213f1ac59ea1ef58f80682187ce6e3a2da72
Chris Zervakis <xrist.zerv@protonmail.com>
2024-05-12 16:25:04 +0300

    fix(nvim): Use mini.surround for LaTeX

--- config/nvim/after/ftplugin/tex.lua
+++ config/nvim/after/ftplugin/tex.lua
@@ -3,34 +3,23 @@ vim.opt_local.spelllang = "en_us,el"
 vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
 
-local ok, surround = pcall(require, "nvim-surround")
-if ok then
-    -- Use via ySe or VSe
-    surround.buffer_setup({
-        surrounds = {
-            -- Surround with LaTeX environment. Better use it with yS or VS
-            -- so that the delimites are placed on new lines
-            ["e"] = {
-                add = function()
-                    local env = require("plugins.nvim-surround").get_input("Environment > ")
-                    if env then
-                        return {
-                            { "\\begin{" .. env .. "}" },
-                            { "\\end{" .. env .. "}" },
-                        }
-                    end
-                end,
-            },
-            -- Surround with LaTeX command
-            ["c"] = {
-                add = function()
-                    local cmd = require("plugins.nvim-surround").get_input("Command >")
-                    return {
-                        { "\\" .. cmd .. "{" },
-                        { "}" },
-                    }
-                end,
-            },
+vim.b.minisurround_config = {
+    custom_surroundings = {
+        -- Surround with LaTeX environment
+        e = {
+            output = function()
+                local env = MiniSurround.user_input("Environment > ")
+                if env then
+                    return { left = "\\begin{" .. env .. "}\n", right = "\n\\end{" .. env .. "}" }
+                end
+            end,
         },
-    })
-end
+        -- Surround with LaTeX command
+        c = {
+            output = function()
+                local cmd = MiniSurround.user_input("Command > ")
+                return { left = "\\" .. cmd .. "{", right = "}" }
+            end,
+        },
+    },
+}
