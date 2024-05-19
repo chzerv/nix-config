3117eeeeb82a9292bcd7a5cafe675e157e1816d8
Chris Zervakis <xrist.zerv@protonmail.com>
2024-05-19 16:20:33 +0300

    Cleanup colorschemes

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,5 @@
 return {
     {
-        "sainnhe/gruvbox-material",
-        lazy = false,
-        enabled = false,
-        priority = 1000,
-        config = function()
-            vim.o.background = "dark"
-            vim.g.gruvbox_material_background = "hard"
-            vim.g.gruvbox_material_better_performance = 1
-            vim.g.gruvbox_material_disable_italic_comment = 0
-            vim.g.gruvbox_material_enable_bold = 0
-            vim.g.gruvbox_material_enable_italic = 1
-            vim.g.gruvbox_material_transparent_background = 0
-            vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
-            vim.cmd.colorscheme("gruvbox-material")
-        end,
-    },
-
-    {
         "catppuccin/nvim",
         lazy = false,
         enabled = true,
