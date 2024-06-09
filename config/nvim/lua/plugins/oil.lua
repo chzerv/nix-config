3bc770b2fefb0391dd19a45b516f5c54deeb2377
Chris Zervakis <xrist.zerv@protonmail.com>
2024-06-09 20:37:19 +0300

    refactor(nvim): Oil tweaks
    + Don't lazy load the plugin so it can be used as a Netrw replacement
      e.g., when using `nvim .`
    + Add a binding for `toggle_float()`

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,6 @@
 return {
-
     "stevearc/oil.nvim",
+    lazy = false,
     opts = {},
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -8,34 +8,15 @@ return {
         {
             "-",
             function()
-                require("oil").open_float()
+                require("oil").open()
+            end,
+        },
+
+        {
+            "<leader>-",
+            function()
+                require("oil").toggle_float()
             end,
         },
     },
-    config = function()
-        require("oil").setup({
-            default_file_explorer = true,
-
-            use_default_keymaps = true,
-
-            keymaps = {
-                ["g?"] = "actions.show_help",
-                ["<CR>"] = "actions.select",
-                ["<C-s>"] = "actions.select_vsplit",
-                ["<C-h>"] = "actions.select_split",
-                ["<C-t>"] = "actions.select_tab",
-                ["<C-p>"] = "actions.preview",
-                ["<C-c>"] = "actions.close",
-                ["<C-l>"] = "actions.refresh",
-                ["-"] = "actions.parent",
-                ["_"] = "actions.open_cwd",
-                ["`"] = "actions.cd",
-                ["~"] = "actions.tcd",
-                ["gs"] = "actions.change_sort",
-                ["gx"] = "actions.open_external",
-                ["g."] = "actions.toggle_hidden",
-                ["g\\"] = "actions.toggle_trash",
-            },
-        })
-    end,
 }
