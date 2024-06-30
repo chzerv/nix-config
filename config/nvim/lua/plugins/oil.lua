fa1aeffea855e660d21ee95a7cc298a9342650a3
Chris Zervakis <xrist.zerv@protonmail.com>
2024-06-30 11:53:45 +0300

    feat(nvim): Toggle detail view in oil.nvim

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -19,4 +19,24 @@ return {
             end,
         },
     },
+    config = function()
+        -- https://github.com/stevearc/oil.nvim/blob/master/doc/recipes.md#toggle-file-detail-view
+        local detail = false
+
+        require("oil").setup({
+            keymaps = {
+                ["gd"] = {
+                    desc = "Toggle file detail view",
+                    callback = function()
+                        detail = not detail
+                        if detail then
+                            require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+                        else
+                            require("oil").set_columns({ "icon" })
+                        end
+                    end,
+                },
+            },
+        })
+    end,
 }
