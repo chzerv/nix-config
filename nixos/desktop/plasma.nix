87fcb1e24ab91f361ecd0cfdb319d6be0c3cba32
Chris Zervakis <xrist.zerv@protonmail.com>
2024-07-03 19:20:25 +0300

    feat: Update Plasma configuration

--- nixos/desktop/plasma.nix
+++ nixos/desktop/plasma.nix
@@ -9,31 +9,35 @@ in {
   config = lib.mkIf opts.desktop.plasma {
     services = {
       xserver = {
-        enable = true;
+        enable = false;
         excludePackages = [
           pkgs.xterm
         ];
-        displayManager = {
-          sddm = {
-            enable = true;
-            wayland.enable = true;
-          };
-          defaultSession = "plasma"; # plasmax11 for an X11 session
-        };
       };
+      displayManager = {
+        sddm = {
+          enable = true;
+          wayland.enable = true;
+        };
+        defaultSession = "plasma";
+      };
+
       desktopManager.plasma6.enable = true;
     };
 
     # KDE packages to uninstall
-    environment.plasma6.excludePackages = with pkgs.kdePackages; [
-      plasma-browser-integration
-      oxygen
+    environment.plasma6.excludePackages = with pkgs; [
+      libsForQt5.ktexteditor
     ];
 
     # KDE packages to install
-    environment.systemPackages = with pkgs.kdePackages; [
-      merkuro
-      kcalc
+    environment.systemPackages = with pkgs; [
+      gtk3
+      gtk4
+      kdePackages.kcalc
+      kdePackages.kfind
+      kdePackages.kleopatra
+      kdePackages.filelight
     ];
 
     # Better integration with GTK apps
