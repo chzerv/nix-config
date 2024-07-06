3f1b2858bfa778f5502122d55417c228b39043f3
Chris Zervakis <xrist.zerv@protonmail.com>
2024-07-06 10:14:56 +0300

    fix(wsl): Docker desktop integration

--- nixos/core/wsl.nix
+++ nixos/core/wsl.nix
@@ -15,10 +15,26 @@
       defaultUser = username;
       startMenuLaunchers = true;
 
-      # Enable integration with Docker Desktop (needs to be installed)
-      docker-desktop.enable = true;
+      # Enable integration with Docker Desktop
+      # https://github.com/nix-community/NixOS-WSL/issues/235#issuecomment-1937424376
+      docker-desktop.enable = false;
+
+      extraBin = with pkgs; [
+        # Binaries for Docker Desktop wsl-distro-proxy
+        {src = "${coreutils}/bin/mkdir";}
+        {src = "${coreutils}/bin/cat";}
+        {src = "${coreutils}/bin/whoami";}
+        {src = "${coreutils}/bin/ls";}
+        {src = "${busybox}/bin/addgroup";}
+        {src = "${su}/bin/groupadd";}
+        {src = "${su}/bin/usermod";}
+      ];
     };
 
+    ## patch the script
+    systemd.services.docker-desktop-proxy.script = lib.mkForce ''${config.wsl.wslConf.automount.root}/wsl/docker-desktop/docker-desktop-user-distro proxy --docker-desktop-root ${config.wsl.wslConf.automount.root}/wsl/docker-desktop "C:\Program Files\Docker\Docker\resources"'';
+
+    # VSCode server workaround
     services.vscode-server.enable = true;
   };
 }
