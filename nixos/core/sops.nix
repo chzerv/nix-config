b60e2865a532b1710a19bbc408db6c0fc5bd426d
Chris Zervakis <xrist.zerv@protonmail.com>
2024-10-21 12:46:14 +0300

    fix(sops): Store key.txt in /var/lib/sops-nix instead of /etc/sops/age

--- nixos/core/sops.nix
+++ nixos/core/sops.nix
@@ -9,7 +9,7 @@
     # Allow overriding `age.keyFile`
     # This is especially useful when we are deploying to a new machine and we want to use
     # it's SSH keys as AGE keys
-    age.keyFile = lib.mkDefault /etc/sops/age/keys.txt;
+    age.keyFile = lib.mkDefault "/var/lib/sops-nix/key.txt";
     age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
   };
 }
