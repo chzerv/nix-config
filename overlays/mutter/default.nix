6a434a73584d620d945864394042f8a5979cade0
Chris Zervakis <xrist.zerv@protonmail.com>
2024-11-30 12:37:47 +0200

    fix(gnome): mutter triple buffering overlay for Gnome 47

---
+++ overlays/mutter/default.nix
@@ -0,0 +1,25 @@
+{pkgs, ...}: (final: prev: {
+  mutter = prev.mutter.overrideAttrs (oldAttrs: {
+    # GNOME dynamic triple buffering (huge performance improvement)
+    # See https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/1441
+    src = final.fetchFromGitLab {
+      domain = "gitlab.gnome.org";
+      owner = "vanvugt";
+      repo = "mutter";
+      rev = "triple-buffering-v4-47";
+      hash = "sha256-JaqJvbuIAFDKJ3y/8j/7hZ+/Eqru+Mm1d3EvjfmCcug=";
+    };
+
+    preConfigure = let
+      gvdb = final.fetchFromGitLab {
+        domain = "gitlab.gnome.org";
+        owner = "GNOME";
+        repo = "gvdb";
+        rev = "2b42fc75f09dbe1cd1057580b5782b08f2dcb400";
+        hash = "sha256-CIdEwRbtxWCwgTb5HYHrixXi+G+qeE1APRaUeka3NWk=";
+      };
+    in ''
+      cp -a "${gvdb}" ./subprojects/gvdb
+    '';
+  });
+})
