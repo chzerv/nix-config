dc609158e9f242d3c46d7248881a1f0eedb4e830
Chris Zervakis <xrist.zerv@protonmail.com>
2025-01-20 20:17:06 +0200

    fix(yazi): Follow upstream changes

--- home/shell/tools/yazi.nix
+++ home/shell/tools/yazi.nix
@@ -7,7 +7,6 @@
         # Pretty much the defaults
         show_hidden = false;
         show_symlink = true;
-        sort_by = "modified";
         sort_dir_first = true;
         sort_sensitive = false;
         sort_reverse = true;
