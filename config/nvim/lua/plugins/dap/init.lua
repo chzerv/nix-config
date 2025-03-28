d1cb16d133a1ee4fdf6d1fe49f6cd8b84aa1ea72
Chris Zervakis <xrist.zerv@protonmail.com>
2025-03-28 09:51:16 +0200

    fix(nvim): Open dap-view automatically when starting a DAP session

--- config/nvim/lua/plugins/dap/init.lua
+++ config/nvim/lua/plugins/dap/init.lua
@@ -85,7 +85,19 @@ return {
         },
     },
     config = function()
-        local dap = require("dap")
+        local dap, dv = require("dap"), require("dap-view")
+        dap.listeners.before.attach["dap-view-config"] = function()
+            dv.open()
+        end
+        dap.listeners.before.launch["dap-view-config"] = function()
+            dv.open()
+        end
+        dap.listeners.before.event_terminated["dap-view-config"] = function()
+            dv.close()
+        end
+        dap.listeners.before.event_exited["dap-view-config"] = function()
+            dv.close()
+        end
 
         -- Setup adapters
         require("plugins.dap.adapters.rust")
