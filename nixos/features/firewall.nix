b056c75759347f23a825eb5197c23f8742dcdf8e
Chris Zervakis <xrist.zerv@protonmail.com>
2025-03-30 12:38:46 +0300

    fix(firewall): Remove unneeded extraCommands

--- nixos/features/firewall.nix
+++ nixos/features/firewall.nix
@@ -5,9 +5,5 @@ in {
   networking.firewall = {
     enable = opts.firewall;
     allowedTCPPorts = sshPorts;
-    extraCommands = ''
-      iptables -A nixos-fw -p tcp --source 192.168.1.12/24 -j nixos-fw-accept
-      iptables -A nixos-fw -p udp --source 192.168.1.12/24 -j nixos-fw-accept
-    '';
   };
 }
