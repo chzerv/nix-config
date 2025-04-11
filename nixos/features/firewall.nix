557e8725bb7c30535783583244a4b45269956d3c
Chris Zervakis <xrist.zerv@protonmail.com>
2025-04-11 10:41:10 +0300

    fix(firewall): Allow localsend

--- nixos/features/firewall.nix
+++ nixos/features/firewall.nix
@@ -1,9 +1,10 @@
 {config, ...}: let
   opts = config.features.nix;
   sshPorts = config.services.openssh.ports;
+  localsendPort = [53317];
 in {
   networking.firewall = {
     enable = opts.firewall;
-    allowedTCPPorts = sshPorts;
+    allowedTCPPorts = sshPorts ++ localsendPort;
   };
 }
