750ec8b3d38e8221908fcce06a7e416369b62e20
Chris Zervakis <xrist.zerv@protonmail.com>
2025-05-28 12:56:50 +0300

    feat(nvim): Markdown related tweaks

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -1,26 +1,9 @@
 -- Markdown enhancements
 return {
-    {
-        "MeanderingProgrammer/markdown.nvim",
-        ft = "markdown",
-        name = "render-markdown",
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            {
-                "echasnovski/mini.icons",
-                opts = {},
-            },
-        },
-        config = function()
-            require("render-markdown").setup({
-                win_options = {
-                    concealcursor = {
-                        -- 'nvic': concealed text is always hidden in normal mode
-                        -- 'empty string': concealed text is shown when in normal mode and the cursor is on the line
-                        rendered = "",
-                    },
-                },
-            })
-        end,
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    ft = "markdown",
+    dependencies = {
+        "saghen/blink.cmp",
     },
 }
